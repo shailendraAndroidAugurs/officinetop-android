f3b282707761daa03d7af5d1dd37efd40e779681
pawandhami289 <pawandhami@augurstech.com>
2020-10-20 13:41:03 +0530

    Android studio or dependancy updated version 4.1

--- app/src/main/java/com/officinetop/officine/cart/CartDetailActivity.kt
+++ app/src/main/java/com/officinetop/officine/cart/CartDetailActivity.kt
@@ -6,6 +6,7 @@ import com.officinetop.officine.R
 
 class CartDetailActivity : AppCompatActivity() {
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cart_detail)
