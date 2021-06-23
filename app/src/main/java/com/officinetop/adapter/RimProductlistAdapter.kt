c6a628a78c79b73c8e8f1f370b79d051fc20fa1e
amitandroidAugurs <69233557+amitandroidAugurs@users.noreply.github.com>
2021-06-23 19:20:37 +0530

    code updated

--- app/src/main/java/com/officinetop/adapter/RimProductlistAdapter.kt
+++ app/src/main/java/com/officinetop/adapter/RimProductlistAdapter.kt
@@ -7,22 +7,21 @@ import android.text.Html
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.RelativeLayout
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.officinetop.R
 import com.officinetop.binding_adapters.imageLoad
 import com.officinetop.data.Models
 import com.officinetop.rim.AvailableRimActivity
+import com.officinetop.rim.RimProductDetailsActivity
 import kotlinx.android.synthetic.main.activity_available_rim.*
 import kotlinx.android.synthetic.main.activity_rim_product_details.*
 import kotlinx.android.synthetic.main.item_faq.view.*
 import kotlinx.android.synthetic.main.item_list_car_availble_rim.view.*
 import kotlinx.android.synthetic.main.item_list_rim_product.view.*
 import kotlinx.android.synthetic.main.item_list_version_car.view.*
+import org.jetbrains.anko.intentFor
 
 class RimProductlistAdapter(private val context: Context, private val productlist: List<Models.rimProductDetails>) : RecyclerView.Adapter<RimProductlistAdapter.ViewHolder>() {
 
@@ -38,8 +37,23 @@ class RimProductlistAdapter(private val context: Context, private val productlis
       holder.tv_type.text = productlist.get(i).Manufacturer
       holder.tv_type_name.text = productlist.get(i).TypeName
       holder.tv_type_color.text = productlist.get(i).TypeColor
-      holder.tv_rim_anteriore.text = productlist.get(i).AlloyRimDescription
       holder.item_price.text = context.getString(R.string.prepend_euro_symbol_string,productlist.get(i).AlloyRimPrice)
+
+        if(!productlist.get(i).rim_posteriore.isNullOrEmpty())
+      holder.tv_rim_posteriore.text = productlist.get(i).rim_posteriore
+        else
+      holder.tv_rim_posteriore.visibility = View.GONE
+
+        if(!productlist.get(i).rim_anteriore.isNullOrEmpty())
+            holder.tv_rim_anteriore.text = productlist.get(i).rim_anteriore
+        else
+            holder.tv_rim_anteriore.visibility = View.GONE
+
+        holder.parent_layout.setOnClickListener {
+        context.startActivity(context.intentFor<RimProductDetailsActivity>().
+        putExtra("front_id",productlist.get(i).AlloyRimFrontID).
+        putExtra("rear_id",productlist.get(i).AlloyRimRearID))
+        }
 
     }
 
@@ -51,6 +65,7 @@ class RimProductlistAdapter(private val context: Context, private val productlis
         val tv_rim_anteriore: TextView = view.tv_rim_anteriore
         val tv_rim_posteriore: TextView = view.tv_rim_posteriore
         val item_price: TextView = view.item_price
+        val parent_layout: LinearLayout = view.parent_layout
 
 
     }
